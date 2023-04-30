3a0f6baae936f976ec79daa297199e33bef8a75c
mimran0303 <112991602+mimran0303@users.noreply.github.com>
2023-04-30 03:14:44 +0000

    Program working

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 
 using namespace std;
 
+const int MAXTHREADS = 2048;
+int nThreads = 0;
+pthread_t ThreadIDs[MAXTHREADS]; //array of thread IDs
+
 static pthread_mutex_t bridge;
 pthread_cond_t signal = PTHREAD_COND_INITIALIZER;
 static int timetoRaiseDrawbridge;
@@ -23,33 +27,41 @@ struct cData {
   int TimeToCross;
 };
 
-pthread_t tid_car;
-pthread_t tid_ship;
 DrawBridgeStatus bridgeStatus = CARSCANGO;
 
 void *Car(void *arglist) {
-  cout << "Car " << arglist << " arrives at the bridge." << endl;
+  cout << "Car " << arglist << " arrives at the bridge and waiting." << endl;
   pthread_mutex_lock(&bridge);
   while (bridgeStatus != CARSCANGO) {
-    cout << "Car " << arglist << " goes across the bridge." << endl;
+    cout << "Car " << arglist << " waiting to cross the bridge." << endl;
     pthread_cond_wait(&signal, &bridge);
   }
-  cout << "Car " << arglist << " is leaving" << endl;
-  pthread_mutex_unlock(&bridge);
+  cout << "Car " << arglist << " is crossing the bridge" << endl;
+  sleep(5);
+  cout<<"Car has finished crossing the bridge"<<endl;
   pthread_cond_signal(&signal);
+  pthread_mutex_unlock(&bridge);
   pthread_exit(NULL);
 }
 
 void *Ship(void *arglist) {
-  cout << "Ship " << arglist << " arrives at the bridge." << endl;
+  cout << "Ship " << arglist << " arrives at the bridge and waiting." << endl;
   pthread_mutex_lock(&bridge);
-  while (bridgeStatus != SHIPSCANGO) {
+  /*while (bridgeStatus != SHIPSCANGO) {
     cout << "Ship " << arglist << " goes under the raised bridge." << endl;
     pthread_cond_wait(&signal, &bridge);
-  }
-  cout << "Ship " << arglist << " is leaving" << endl;
+  }*/
+  cout<<"Ship is raising the bridge"<<endl;
+  sleep(1);
+  bridgeStatus = SHIPSCANGO;
+  cout << "Ship " << arglist << " goes under the bridge" << endl;
+  sleep(10);
+  cout<<"Ship has finished crossing the bridge"<<endl;
+  sleep(1);
+  cout<<"Bridge is lowered and cars can go"<<endl;
+  bridgeStatus = CARSCANGO;
+   pthread_cond_signal(&signal);
   pthread_mutex_unlock(&bridge);
-  pthread_cond_signal(&signal);
   pthread_exit(NULL);
 }
 
@@ -107,7 +119,9 @@ int main() {
       // cout << "####CAR THREAD DETECTED####_ca" << endl;
       pthread_t tid;
       struct cData *data;
-      pthread_create(&tid_car, NULL, Car, (void *)&data);
+      pthread_create(&tid, NULL, Car, (void *)&data);
+      ThreadIDs[nThreads] = tid;
+      nThreads++;
     }
     if (strcasecmp(comp, "Ship") == 0) {
       // insert some function that will recognize this as a thread and start
@@ -116,12 +130,16 @@ int main() {
       // cout << "^^^^Ship THREAD DETECTED^^^^" << endl;
       pthread_t tid;
       struct cData data;
-      pthread_create(&tid_ship, NULL, Ship, (void *)&data);
+      pthread_create(&tid, NULL, Ship, (void *)&data);
+      ThreadIDs[nThreads] = tid;
+      nThreads++;
     }
   }
 
-  pthread_join(tid_car, NULL);
-  pthread_join(tid_ship, NULL);
+  for(int i = 0; i<nThreads;i++)
+  {
+    pthread_join(ThreadIDs[i], NULL);
+  }
 
   // from vector threads can be constructed
 }
